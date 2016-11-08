06246aced063ce78d45f4194654df5d62b910d94
Hethix <An.ryge@gmail.com>
2016-11-08 13:42:34 +0100

    Update

---
+++ Baseball/Assets/Scripts/BallHandler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallHandler : MonoBehaviour {
+
+    public bool ballBeenHit;
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start () {
+        ballBeenHit = false;
+
+        coroutine = DestroyBall(1.0f);
+        StartCoroutine(coroutine);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        //ballBeenHit = true;
+        Debug.Log("Velocity magnitude: " + col.relativeVelocity.magnitude); //This will give a number which decides how much vibration we should use
+    }
+
+    private IEnumerator DestroyBall(float time)
+    {
+        Debug.Log("Started DestroyBalll");
+        yield return new WaitForSeconds(time);
+        if (ballBeenHit)
+        {
+            yield return new WaitForSeconds(time * 5);
+            Debug.Log("Trying to destroy it now");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("Trying to destroy it now");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Baseball/Assets/Scripts/BallThrower.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallThrower : MonoBehaviour {
+
+    private float ballVelocity;
+    public GameObject baseball;
+    private GameObject ballClone;
+
+    private bool ballSpawned;
+    private IEnumerator coroutine;
+
+	// Use this for initialization
+	void Start () {
+        ballVelocity = 5.0f;
+        ballSpawned = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!ballSpawned && Input.GetKeyDown(KeyCode.A)) //Needs to be a button on the controller!!! Will implement when I have the Vive.
+        {
+            ballSpawned = true;
+            coroutine = SpawnTimer(1.0f);
+            StartCoroutine(coroutine);
+            ballClone = (GameObject) Instantiate(baseball, transform.position, transform.rotation);
+            ballClone.GetComponent<Rigidbody>().velocity = transform.up * ballVelocity;
+            Debug.Log("Ball spawned");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ballClone.GetComponent<Rigidbody>().velocity = transform.forward * ballVelocity;
+            ballClone.GetComponent<BallHandler>().ballBeenHit = true;
+            Debug.Log("I Hit the ball");
+        }
+    }
+
+    
+    private IEnumerator SpawnTimer(float time)
+    {
+        Debug.Log("Started SpawnTimer");
+        yield return new WaitForSeconds(time);
+        ballSpawned = false;
+    } 
+}
