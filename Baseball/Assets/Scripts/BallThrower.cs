114753bf3079d157045df33f8d35f589c446c5d0
AAU_SmallLab_PC <An.ryge@gmail.com>
2016-11-14 13:26:21 +0100

    Spawn on Trigger.
    Should work but gives null reference..

--- Baseball/Assets/Scripts/BallThrower.cs
+++ Baseball/Assets/Scripts/BallThrower.cs
@@ -6,20 +6,32 @@ public class BallThrower : MonoBehaviour {
     private float ballVelocity;
     public GameObject baseball;
     private GameObject ballClone;
-
+  
     private bool ballSpawned;
     private IEnumerator coroutine;
 
-	// Use this for initialization
-	void Start () {
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+
+    // Use this for initialization
+    void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+        device = SteamVR_Controller.Input((int)trackedObject.index);
         ballVelocity = 8.0f;
         ballSpawned = false;
     }
 
     // Update is called once per frame
-    void Update () {
-        if (!ballSpawned && Input.GetKeyDown(KeyCode.A)) //Needs to be a button on the controller!!! Will implement when I have the Vive.
+    void Update ()
+    {
+        
+
+        if (!ballSpawned && device.GetPress(triggerButton) ) //Needs to be a button on the controller!!! Will implement when I have the Vive.
         {
+            Debug.Log("Trigger Pressed");
+                
             coroutine = SpawnTimer(1.0f);
             StartCoroutine(coroutine);
             ballClone = (GameObject) Instantiate(baseball, transform.position, transform.rotation);
--- Baseball/Assets/Scripts/BatHandler.cs
+++ Baseball/Assets/Scripts/BatHandler.cs
@@ -5,20 +5,27 @@ public class BatHandler : MonoBehaviour {
 
     public Transform controllerPosition;
 
+    private Vector3 previousPosition;
+    private Vector3 velocity;
+
 	// Use this for initialization
 	void Start () {
+        previousPosition = transform.position;
         transform.position = controllerPosition.position;
         transform.SetParent(controllerPosition);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        velocity = (transform.position - previousPosition) / Time.deltaTime;
+        previousPosition = transform.position;
+
+    }
 
 
     void OnCollisionEnter(Collision col)
     {
-        Debug.Log("Velocity magnitude: " + col.relativeVelocity.magnitude); //This will give a number which decides how much vibration we should use
+        Debug.Log("Velocity of bat: " + velocity + " " + "Velocity magnitude: " + col.relativeVelocity.magnitude); //This will give a number which decides how much vibration we should use
+        col.rigidbody.AddRelativeForce(velocity);
     }
 }
